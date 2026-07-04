b947ee5bb6f5adcf348696eabf70e3d39a191bd3
deniardiyanto <deniardi4@gmail.com>
2026-07-05 05:08:42 +0700

    done adjust 6.4C

--- src/SmartSchool.API/obj/Debug/net8.0/SmartSchool.API.AssemblyInfo.cs
+++ src/SmartSchool.API/obj/Debug/net8.0/SmartSchool.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartSchool.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d2a1c6fda3c4c8f8878d5be5d184aa54a14971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050e72b47eb4d86d9d1c7fe7045bf10c6168659a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartSchool.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartSchool.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
